f6916f4e334191ba81a453a3842c84182efb3735
ChaVi6 <vitaliychaplin@gmail.com>
2021-11-27 01:08:15 +0300

    Решеил задачу findSumOfTwo

--- src/lesson5/task1/Map.kt
+++ src/lesson5/task1/Map.kt
@@ -165,9 +165,9 @@ fun whoAreInBoth(a: List<String>, b: List<String>): List<String> = TODO()
  *     mapOf("Emergency" to "911", "Police" to "02")
  *   ) -> mapOf("Emergency" to "112, 911", "Police" to "02")
  */
-fun mergePhoneBooks(mapA: Map<String, String>, mapB: Map<String, String>): Map<String, String> = TODO()
-
-
+fun mergePhoneBooks(mapA: Map<String, String>, mapB: Map<String, String>) {
+    TODO()
+}
 
 /**
  * Средняя (4 балла)
@@ -179,8 +179,7 @@ fun mergePhoneBooks(mapA: Map<String, String>, mapB: Map<String, String>): Map<S
  *   averageStockPrice(listOf("MSFT" to 100.0, "MSFT" to 200.0, "NFLX" to 40.0))
  *     -> mapOf("MSFT" to 150.0, "NFLX" to 40.0)
  */
-fun averageStockPrice(stockPrices: List<Pair<String, Double>>): Map<String, Double> = TODO()
-
+fun averageStockPrice(stockPrices: List<Pair<String, Double>>): Nothing = TODO()
 /**
  * Средняя (4 балла)
  *
@@ -289,8 +288,14 @@ fun propagateHandshakes(friends: Map<String, Set<String>>): Map<String, Set<Stri
  *   findSumOfTwo(listOf(1, 2, 3), 4) -> Pair(0, 2)
  *   findSumOfTwo(listOf(1, 2, 3), 6) -> Pair(-1, -1)
  */
-fun findSumOfTwo(list: List<Int>, number: Int): Pair<Int, Int> = TODO()
-
+fun findSumOfTwo(list: List<Int>, number: Int): Pair<Int, Int> {
+    for (i in list.indices) {
+        if ((number - list[i] in list) && (i != list.indexOf(number - list[i]))) {
+            return Pair(minOf(i, list.indexOf(number - list[i])), maxOf(i, list.indexOf(number - list[i])))
+        }
+    }
+    return Pair(-1, -1)
+}
 /**
  * Очень сложная (8 баллов)
  *
